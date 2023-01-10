a339af68b42b53f020db7af07cb44914358b8885
zjhayes <zachary.j.hayes@gmail.com>
2023-01-10 00:09:26 -0600

    Add production progression bars to task details. Update task detail and task panels to populate data from task.

--- Assets/Scripts/UI/TaskPanel.cs
+++ Assets/Scripts/UI/TaskPanel.cs
@@ -1,13 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TaskPanel : MonoBehaviour
 {
     [SerializeField]
     private Task task;
+    [SerializeField]
+    Image taskTypeIcon;
+    [SerializeField]
+    TextMeshProUGUI summaryText;
+    [SerializeField]
+    Image assigneeImage;
+    [SerializeField]
+    Sprite defaultAssigneePortrait;
+    [SerializeField]
+    TextMeshProUGUI storyPointsText;
 
     public Task Task
     {
         get { return task; }
         set { task = value; }
+    }
+
+    void Start()
+    {
+        UpdateDetails();
+
+    }
+
+    void UpdateDetails()
+    {
+        taskTypeIcon.sprite = task.TaskTypeIcon;
+        summaryText.text = task.Summary;
+        storyPointsText.text = task.StoryPoints.ToString();
+
+        if(task.Assignee)
+        {
+            assigneeImage.sprite = task.Assignee?.Portrait;
+        }
+        else
+        {
+            assigneeImage.sprite = defaultAssigneePortrait;
+        }
     }
 }
