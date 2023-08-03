b6290fdad0ad0c2673205cf0d1c856169c14f0f6
zjhayes <zachary.j.hayes@gmail.com>
2023-08-03 18:25:37 -0500

    Place speech bubbles on separate object from selected character icon. Add PROD label to Production Server prefab.

--- Assets/Scripts/UI/Overhead/OverheadCanvasController.cs
+++ Assets/Scripts/UI/Overhead/OverheadCanvasController.cs
@@ -3,21 +3,23 @@ using UnityEngine;
 public class OverheadCanvasController : CanvasController
 {
     [SerializeField]
-    GameObject idleSpeechBubblePrefab;
+    private Transform speechBubbleParent;
     [SerializeField]
-    GameObject frustrationSpeechBubblePrefab;
+    private GameObject idleSpeechBubblePrefab;
+    [SerializeField]
+    private GameObject frustrationSpeechBubblePrefab;
 
     public SpeechBubble CreateIdleSpeechBubble()
     {
         GameObject idleSpeechBubble = Instantiate(idleSpeechBubblePrefab);
-        idleSpeechBubble.transform.SetParent(transform); // Set parent to canvas.
+        idleSpeechBubble.transform.SetParent(speechBubbleParent); // Set parent to canvas.
         return idleSpeechBubble.GetComponent<SpeechBubble>();
     }
 
     public SpeechBubble CreateFrustrationSpeechBubble()
     {
         GameObject frustrationSpeechBubble = Instantiate(frustrationSpeechBubblePrefab);
-        frustrationSpeechBubble.transform.SetParent(transform); // Set parent to canvas.
+        frustrationSpeechBubble.transform.SetParent(speechBubbleParent); // Set parent to canvas.
         return frustrationSpeechBubble.GetComponent<SpeechBubble>();
     }
 }
