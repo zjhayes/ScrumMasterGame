e74f800aeac6d450a626b5c642fb284fa59a886c
zjhayes <zachary.j.hayes@gmail.com>
2023-10-16 20:43:54 -0500

    Add cartridge apperance, updates based on story type.

---
+++ Assets/Scripts/Interactables/Pickups/CartridgeAppearance.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Cartridge))]
+public class CartridgeAppearance : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer storyIcon;
+    [SerializeField]
+    private Renderer cartridgeRenderer;
+
+    [SerializeField]
+    private Color taskColor;
+    [SerializeField]
+    private Sprite taskIcon;
+    [SerializeField]
+    private Color bugColor;
+    [SerializeField]
+    private Sprite bugIcon;
+    [SerializeField]
+    private Color requestColor;
+    [SerializeField]
+    private Sprite requestIcon;
+
+    private Cartridge cartridge;
+
+    private void Awake()
+    {
+        cartridge = GetComponent<Cartridge>();
+        cartridge.OnStoryUpdated += UpdateAppearance;
+    }
+
+    private void UpdateAppearance()
+    {
+        SetFor(cartridge.Story.Details.Type);
+    }
+
+    private void SetFor(StoryType type)
+    {
+        if(type == StoryType.TASK)
+        {
+            Set(taskColor, taskIcon);
+        }
+        else if(type == StoryType.BUG)
+        {
+            Set(bugColor, bugIcon);
+        }
+        else if(type == StoryType.REQUEST)
+        {
+            Set(requestColor, requestIcon);
+        }
+    }
+
+    private void Set(Color color, Sprite icon)
+    {
+        cartridgeRenderer.materials[0].color = color;
+        storyIcon.sprite = icon;
+    }
+}
