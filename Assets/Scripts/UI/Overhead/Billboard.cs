ac68164558f87f4bef0a3adaa9e35779bfd276d6
zjhayes <zachary.j.hayes@gmail.com>
2025-01-16 18:07:36 -0600

    Fix Billboarding on Overworld camera

--- Assets/Scripts/UI/Overhead/Billboard.cs
+++ Assets/Scripts/UI/Overhead/Billboard.cs
@@ -4,7 +4,7 @@ public class Billboard : GameBehaviour
 {
     public void FaceCamera()
     {
-        transform.LookAt(transform.position + gameManager.Camera.MainCamera.transform.rotation * Vector3.forward,
+        transform.LookAt(transform.position + gameManager.Camera.OverworldCameraTransform.rotation * Vector3.forward,
                      gameManager.Camera.MainCamera.transform.rotation * Vector3.up);
     }
 }
